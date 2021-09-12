17cf75c925e5c5e67bf0ca5c009fd3ab1db97255
bjorn-iversen <bjorn.iversen@knowit.no>
2021-09-12 16:09:12 +0200

    feature: add potentially working enemy spawner

--- Assets/scripts/EnemySpawner.cs
+++ Assets/scripts/EnemySpawner.cs
@@ -4,27 +4,50 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour
 {
     public MapGeneration mapGenerator;
+    public BaseEnemyScript enemyScript; 
     private int spawnX;
     private int spawnY;
+    private float spawnInterval;
+    private float timeSinceLastSpawn;
     // Use this for initialization
     void Start()  
     {
         System.Random rng = new System.Random();
-        bool foundSpawn = false;
-        while (!foundSpawn)
-        {
-            spawnX = rng.Next(0, mapGenerator.getXCells());
-            spawnY = rng.Next(0, mapGenerator.getYCells());
-            if (mapGenerator.getCellAt(spawnX, spawnY) != null)
-            {
-                foundSpawn = true;
-            }
-        }
+        findAndSetSpawn(mapGenerator.getXCells(), mapGenerator.getYCells());
+        spawnEnemy();
+        timeSinceLastSpawn = 0;
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= spawnInterval)
+        {
+            spawnEnemy();
+            timeSinceLastSpawn = 0;
+        }
+    }
 
+    public void findAndSetSpawn(int width, int height)
+    {
+        bool hasFoundSpawn = false;
+        System.Random rng = new System.Random();
+        while (!hasFoundSpawn)
+        {
+            spawnX = rng.Next(0, width);
+            spawnY = rng.Next(0, height);
+            if (mapGenerator.getCellAt(spawnX, spawnY) != null)
+            {
+                hasFoundSpawn = true;
+            }
+        }
+    }
+
+    public void spawnEnemy()
+    {
+        BaseEnemyScript spawnedEnemy = Instantiate(enemyScript);
+        spawnedEnemy.initializeEnemy(mapGenerator.getCellAt(spawnX, spawnY));
     }
 }
