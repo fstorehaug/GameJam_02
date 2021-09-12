8dda93fd03cf5b26503ba96a3084d4b89959b142
bjorn-iversen <bjorn.iversen@knowit.no>
2021-09-12 15:34:51 +0200

    feature: add initial enemy spawner

---
+++ Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public MapGeneration mapGenerator;
+    private int spawnX;
+    private int spawnY;
+    // Use this for initialization
+    void Start()  
+    {
+        System.Random rng = new System.Random();
+        bool foundSpawn = false;
+        while (!foundSpawn)
+        {
+            spawnX = rng.Next(0, mapGenerator.getXCells());
+            spawnY = rng.Next(0, mapGenerator.getYCells());
+            if (mapGenerator.getCellAt(spawnX, spawnY) != null)
+            {
+                foundSpawn = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
