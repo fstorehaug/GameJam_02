a26aaa61359743b5fd72baaeec987584d5a6e6e0
Fredrik Storehaug <fredrik@varjo.com>
2021-09-18 12:54:32 +0200

    added main menu sceene, added scriptable object for settings

---
+++ Assets/Settings/SettingsScriptableObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScriptableObject : MonoBehaviour
+{
+    [SerializeField]
+    private static int _mapWidth;
+
+    [SerializeField]
+    private static int _mapHeight;
+
+    [SerializeField]
+    private static int _spawnRate; 
+
+
+    public static void SetMapDimentions(int width, int height)
+    {
+        _mapHeight = height;
+        _mapWidth = width;
+    }
+
+    public static void SetSpawnRate(int spawnrate)
+    {
+        _spawnRate = spawnrate;
+    }
+
+    public static int GetMapHeight()
+    {
+        return _mapHeight;
+    }
+
+    public static int GetMapWidht()
+    {
+        return _mapWidth;
+    }
+
+    public static int GetSpawnRate()
+    {
+        return _spawnRate;
+    }
+
+}
