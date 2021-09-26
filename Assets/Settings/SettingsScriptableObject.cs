c9c6c091212a31525e25941b111cfb23d6758f16
Fredrik Storehaug <fredrik@varjo.com>
2021-09-26 14:23:13 +0200

    added default values and clamping on settings

--- Assets/Settings/SettingsScriptableObject.cs
+++ Assets/Settings/SettingsScriptableObject.cs
@@ -12,13 +12,20 @@ public class SettingsScriptableObject : ScriptableObject
     private static int _mapHeight;
 
     [SerializeField]
-    private static int _spawnRate; 
+    private static int _spawnRate;
 
+    private const int _maxMapWidht = 40;
+    private const int _minMapWidth = 10;
+    private const int _defaultMapWidht = 20;
+
+    private const int _maxMapHeight = 40;
+    private const int _minMapHeight = 10;
+    private const int _defaultMapHeight = 20;
 
     public static void SetMapDimentions(int width, int height)
     {
-        _mapHeight = height;
-        _mapWidth = width;
+        _mapHeight = Mathf.Clamp(height, _minMapHeight, _maxMapHeight);
+        _mapWidth = Mathf.Clamp(width, _minMapWidth, _maxMapWidht);
     }
 
     public static void SetSpawnRate(int spawnrate)
@@ -28,16 +35,25 @@ public class SettingsScriptableObject : ScriptableObject
 
     public static int GetMapHeight()
     {
+        if (_mapHeight < _maxMapHeight || _mapHeight > _minMapHeight)
+            _mapHeight = _defaultMapHeight;
+
         return _mapHeight;
     }
 
     public static int GetMapWidht()
     {
+        if (_mapWidth < _maxMapWidht || _mapWidth > _minMapWidth)
+            _mapWidth = _defaultMapWidht;
+
         return _mapWidth;
     }
 
     public static int GetSpawnRate()
     {
+        if (_spawnRate < 50 || _spawnRate > 500)
+            _spawnRate = 100;
+
         return _spawnRate;
     }
 
