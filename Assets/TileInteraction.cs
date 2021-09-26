5bfdd857c92d32e65617d73c18333092d6146b0f
Fredrik Storehaug <fredrik@varjo.com>
2021-09-26 13:20:32 +0200

    added inputactions and raplaced onmouseclick

---
+++ Assets/TileInteraction.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TileInteraction : MonoBehaviour
+{
+    private PlayerInput inputActions;
+
+    private void Start()
+    {
+        inputActions = new PlayerInput();
+        inputActions.TileInteraction.Enable();
+
+        inputActions.TileInteraction.OpenTile.performed += OpenTile_performed;
+
+    }
+
+    private void OpenTile_performed(InputAction.CallbackContext obj)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(inputActions.TileInteraction.MousePosition.ReadValue<Vector2>());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (hit.collider.gameObject.tag.ToLower().Contains("livercell"))
+            {
+                hit.collider.gameObject.transform.parent.gameObject.GetComponent<LiverCell>().ToogleCell();
+            }
+        }
+    }
+
+}
