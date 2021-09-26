fe2d3af54faec02913fd90ccca53b83874f58778
Vegard Hagen <vegard@edgeworks.no>
2021-09-26 18:26:44 +0200

    feat: enemies now kinda rotate in the direction they're travelling.

--- Assets/scripts/BaseEnemyScript.cs
+++ Assets/scripts/BaseEnemyScript.cs
@@ -11,13 +11,16 @@ public sealed class BaseEnemyScript : MonoBehaviour
     private readonly float _speed = 1f;
     private readonly int _baseDamage = 30;
 
+    private Coroutine turnCoroutine;
+
     public void InitializeEnemy(LiverCell spawnCell)
     {
         previousCell = spawnCell;
         currentCell = spawnCell;
         nextCell = spawnCell;
         var position = nextCell.transform.position;
-        transform.position = new Vector3(position.x, position.y, position.z - .2f);
+        transform.position = new Vector3(position.x, position.y, -0.08f);
+        transform.rotation = new Quaternion(0, 0, 0, 0);
     }
 
     private void Update()
@@ -35,6 +38,28 @@ public sealed class BaseEnemyScript : MonoBehaviour
         previousCell = currentCell;
         currentCell = nextCell;
         nextCell = FindNextCell();
+
+        var lookRotation = (transform.position - nextCell.transform.position).normalized;
+        var targetRotation = Quaternion.LookRotation(
+            new Vector3(lookRotation.x, lookRotation.y, 0f),
+            Vector3.forward);
+
+        StopAllCoroutines(); 
+        turnCoroutine = StartCoroutine(RotateTowardsEnumerator(targetRotation));
+    }
+
+    private IEnumerator RotateTowardsEnumerator(Quaternion targetRotation)
+    {
+        const float stepAngle = 0.1f;
+
+        for (var i = 0; i < 256; i++)
+        {
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, targetRotation, stepAngle/Time.deltaTime);
+            yield return 0;
+        }
+
+        yield return 0;
     }
 
     private void UpdatePosition()
@@ -47,7 +72,7 @@ public sealed class BaseEnemyScript : MonoBehaviour
         var nextPosition = Vector2.MoveTowards(enemyPosition, destination, _speed * Time.deltaTime);
 
         position = nextPosition;
-        transform.position = position + new Vector3(0f, 0f, -0.2f);
+        transform.position = position + new Vector3(0f, 0f, -0.08f);
     }
 
     private LiverCell FindNextCell()
@@ -59,7 +84,6 @@ public sealed class BaseEnemyScript : MonoBehaviour
         TerminateSelf();
         nextCell = null;
         return null;
-
     }
 
     private void OnCellArrival()
