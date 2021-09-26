e851af70da4ea3f0d4b4d0c4f5deb748b35e6fd8
Fredrik Storehaug <fredrik@varjo.com>
2021-09-26 14:03:08 +0200

    fixed camara controll

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        SetPosition(SettingsScriptableObject.GetMapWidht(), SettingsScriptableObject.GetMapHeight());    
+    }
+    private void SetPosition(float widht, float height) 
+    {
+        transform.position = new Vector3((widht / 2) - 0.5f, (height - widht * 0.5f) / 2, - (Mathf.Max(widht, height)*(Mathf.Atan(Camera.main.fieldOfView/360))));   
+    }
 }
